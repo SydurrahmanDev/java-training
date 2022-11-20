fb0016fde96d3c40e98e0b7d221c20569a1cd3f3
unknown <sydur.dev@gmail.com>
2022-11-20 11:24:16 -0500

    this is my first commit

---
+++ src/com/abc/java/training/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.abc.java.training;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("My Name is Sydur Rahman");
+
+
+
+    }
+}
